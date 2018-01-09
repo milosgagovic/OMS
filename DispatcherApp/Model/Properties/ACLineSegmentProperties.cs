1b7accfcc6f6cde4ece4c33443d358f5db7deb01
AleksandraMisic <alexmisic94@gmail.com>
2018-01-10 00:23:12 +0100

    Client - iscrtavanje laznih podataka

---
+++ DispatcherApp/Model/Properties/ACLineSegmentProperties.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DispatcherApp.Model.Properties
+{
+    public class ACLineSegmentProperties : Properties
+    {
+        public float Lenght { get; private set; }
+    }
+}
