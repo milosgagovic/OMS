9510b0d45b1c521233babb21e43357ee179e13a1
milosgagovic <gagovicmilosgagovic@gmail.com>
2018-01-09 22:40:52 +0100

    Added N-Tree struct of data in memory

---
+++ Common/DMSCommon/TreeGraph/NodeLink.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DMSCommon.TreeGraph
+{
+    namespace Tree
+    {
+        public class NodeLink
+        {
+            public long Id { get; set; }
+            public long? Parent { get; set; }
+            public long? Next { get; set; }
+            public long? Child { get; set; }
+            public int Depth { get; set; }
+        }
+    }
+}
